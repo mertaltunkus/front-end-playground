7f899c82c81349b80fbaadc5d27b1202f90d2952
mertaltunkus <mertaltunkus@sabanciuniv.edu>
2020-07-09 16:31:30 +0300

    commit2

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = new Vue({
+    el:"#vue-app",
+    data:{
+        accord1:false,
+        accord2:false,
+        accord3:false
+
+    },
+    methods:{
+
+    },
+    computed:{
+
+    }
+
+})
